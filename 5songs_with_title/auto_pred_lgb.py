838a799a37c55678b4d06f4c0cd77cbac25405da
Bowen He <bhe@ucdavis.edu>
2018-07-01 10:02:01 +0800

    Create auto_pred_lgb.py
    file to generate the final results for 5 songs with title sub task.

---
+++ 5songs_with_title/auto_pred_lgb.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("python main.py")
+os.system("python lgb_features.py")
+os.system("python lgb_train.py")
+os.system("python prediction.py 2 1")
+os.system("python lgb_test_features.py")
+os.system("python lgb_predict.py 2")
